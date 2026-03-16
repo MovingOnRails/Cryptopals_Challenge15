a568ce9dc9dc8aa82256085742be0d62b3729bfb
first <first@windows.windows>
2026-03-16 17:52:40 -0300

    Added function for it to work with given length instead of strlen

--- main.c
+++ main.c
@@ -31,3 +31,32 @@ bool validatePadding(unsigned char string[], unsigned char** result, int* number
     
     return bytesRead == numberOfPaddingBytes;
 }
+
+bool validatePadding_withLength(unsigned char string[], int stringLength, unsigned char** result, int* numberOfBytesWritten){
+
+    int BLOCK_SIZE = 16;
+    int paddingLength = 0;
+    if(stringLength % BLOCK_SIZE != 0){
+        return false;
+    }
+
+    int numberOfPaddingBytes = string[stringLength-1];
+    int bytesRead = 0;
+    for(int i=0;i<numberOfPaddingBytes;i++){
+        if(string[stringLength-i-1] == numberOfPaddingBytes){
+            bytesRead++;
+        }
+    }
+
+    
+    if(bytesRead == numberOfPaddingBytes){
+        (*numberOfBytesWritten) = stringLength-numberOfPaddingBytes;
+        unsigned char* tmpResult = calloc(*numberOfBytesWritten,sizeof(unsigned char));
+        for(int i=0;i<stringLength-numberOfPaddingBytes;i++){
+            tmpResult[i] = string[i];
+        }
+        (*result) = tmpResult;
+    }
+    
+    return bytesRead == numberOfPaddingBytes;
+}
